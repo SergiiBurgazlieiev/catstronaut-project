bd1faabc07dd05afc89616f7ce9a3f8dd5e14b12
Sergii Burgazlieiev <burgazlieiev@gmail.com>
2024-07-08 21:19:00 -0500

    codegen configuration

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import GlobalStyles from './styles';
 import Pages from './pages';
+
+const client = new ApolloClient({
+	uri: 'http://localhost:4000',
+	cache: new InMemoryCache(),
+});
 
 const root = createRoot(document.getElementById('root')!);
 
 root.render(
-  <React.StrictMode>
-    <GlobalStyles />
-    <Pages />
-  </React.StrictMode>
+	<ApolloProvider client={client}>
+		<React.StrictMode>
+			<GlobalStyles />
+			<Pages />
+		</React.StrictMode>
+	</ApolloProvider>
 );
