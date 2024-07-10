22157bd4c139fc9af09a5ea2f9993dec16204f68
Sergii Burgazlieiev <burgazlieiev@gmail.com>
2024-07-09 23:35:01 -0500

    updated config.mappers - replaces a GraphQL type usage with a custom type, allowing us to return custom object from our resolvers - ModuleModel

--- server/src/datasources/track-api.ts
+++ server/src/datasources/track-api.ts
@@ -1,5 +1,5 @@
 import { RESTDataSource } from '@apollo/datasource-rest';
-import { TrackModel, AuthorModel } from '../models';
+import { TrackModel, AuthorModel, ModuleModel } from '../models';
 
 export class TrackAPI extends RESTDataSource {
 	baseURL = 'https://odyssey-lift-off-rest-api.herokuapp.com/';
@@ -13,10 +13,12 @@ export class TrackAPI extends RESTDataSource {
 	}
 
 	getTrack(trackId: string) {
-		return this.get(`track/${encodeURIComponent(trackId)}`);
+		return this.get<TrackModel>(`track/${encodeURIComponent(trackId)}`);
 	}
 
 	getTrackModules(trackId: string) {
-		return this.get(`track/${encodeURIComponent(trackId)}/modules`);
+		return this.get<ModuleModel[]>(
+			`track/${encodeURIComponent(trackId)}/modules`
+		);
 	}
 }
